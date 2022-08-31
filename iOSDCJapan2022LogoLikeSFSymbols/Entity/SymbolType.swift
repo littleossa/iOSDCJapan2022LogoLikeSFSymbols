ad107d2d53a2afa94131183c76b97a5270dc0aa2
littleossa <littleossa@gmail.com>
2022-08-31 15:20:04 +0900

    [fix] Fix positions of each symbols

--- iOSDCJapan2022LogoLikeSFSymbols/Entity/SymbolType.swift
+++ iOSDCJapan2022LogoLikeSFSymbols/Entity/SymbolType.swift
@@ -19,24 +19,26 @@ enum SymbolType: String, Identifiable, CaseIterable {
         switch self {
         case .green:
             return SymbolAppearance(color: .green,
-                                sizeMagnification: 0.26,
-                                offsetXMagnification: -0.09,
-                                offsetYMagnification: 0.14)
+                                sizeMagnification: 0.25,
+                                offsetXMagnification: 0.1,
+                                    offsetYMagnification: -0.15)
+            
         case .orange:
             return SymbolAppearance(color: .orange,
                                 sizeMagnification: 0.2,
-                                offsetXMagnification: 0.07,
-                                offsetYMagnification: -0.19)
+                                offsetXMagnification: -0.07,
+                                offsetYMagnification: 0.17)
         case .blue:
             return SymbolAppearance(color: .blue,
-                                sizeMagnification: 0.17,
-                                       offsetXMagnification: 0.25,
-                                offsetYMagnification: 0.086)
+                                sizeMagnification: 0.16,
+                                    offsetXMagnification: -0.22,
+                                    offsetYMagnification: -0.12)
+            
         case .punk:
             return SymbolAppearance(color: .pink,
-                                sizeMagnification: 0.11,
-                                offsetXMagnification: -0.18,
-                                offsetYMagnification: -0.15)
+                                sizeMagnification: 0.12,
+                                offsetXMagnification: 0.2,
+                                    offsetYMagnification: 0.15)
         }
     }
     
